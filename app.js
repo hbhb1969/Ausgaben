dc84cdf67abd4f004c75c6fb7a3aa05ad803404e
hbhb1969 <37511683+hbhb1969@users.noreply.github.com>
2018-04-08 17:12:17 +0200

    connections -> pool

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const insert = require('./db/insert');
-const connections = require('./db/connections');
+const pool = require('./db/pool');
 const app = express();
 app.use(express.static('public'));
 app.use(bodyParser.json());
@@ -17,14 +17,13 @@ app.post('/buchen', (req, res) => {
 });
 // Route zum Füllen eines Select Feldes
 app.get('/select', (req, res) => {
-  connections.query('SELECT * FROM ausgaben_art ORDER BY bezeichnung', function(
-    err,
-    rows,
-    fields
-  ) {
-    if (err) throw err;
-    res.json(rows); // response = Abfrageergebnis im JSON-Format -> wird in der der HTML-Seite per fetch abgerufen
-  });
+  pool.query(
+    'SELECT * FROM ausgaben_art ORDER BY bezeichnung',
+    (err, rows, fields) => {
+      if (err) throw err;
+      res.json(rows); // response = Abfrageergebnis im JSON-Format -> wird in der der HTML-Seite per fetch abgerufen
+    }
+  );
 });
 app.listen(7555, () => {
   console.log('Server running on http://localhost:7555');
