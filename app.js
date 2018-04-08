8dfdf2b71f828f1ad7dc9508df4841d64c76fe36
hbhb1969 <37511683+hbhb1969@users.noreply.github.com>
2018-04-08 14:40:09 +0200

    Select befüllt

--- app.js
+++ app.js
@@ -14,6 +14,25 @@ app.post('/buchen', (req, res) => {
     })
     .then(() => res.sendStatus(200));
 });
+app.get('/select', function(req, res) {
+  var mysql = require('mysql');
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'passwort',
+    database: 'ausgaben'
+  });
+  connection.connect();
+  connection.query('SELECT * FROM ausgaben_art ORDER BY bezeichnung', function(
+    err,
+    rows,
+    fields
+  ) {
+    connection.end();
+    if (err) throw err;
+    res.json(rows);
+  });
+});
 app.listen(7555, () => {
   console.log('Server running on http://localhost:7555');
 });
